2b9a4233b3b3993d0c4a71e6e21edbdfc3746cb3
Danco55 <dancomihal5@gmail.com>
2023-12-19 20:54:41 +0100

    Added converter to repository

---
+++ index.js
@@ -0,0 +1,25 @@
+const celsiusEl = document.getElementById("celsius");
+const fahrenheitEl = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin"); 
+
+function computeTemp(event) {
+  const currentValue = +event.target.value;
+
+  switch (event.target.name) {
+    case "celsius":
+      kelvinEl.value = (currentValue + 273.15).toFixed(2);
+      fahrenheitEl.value = (currentValue * 9/5 + 32).toFixed(2);
+      break;
+    case "fahrenheit":
+      celsiusEl.value = ((currentValue - 32) * 5/9).toFixed(2);
+      kelvinEl.value = ((currentValue - 32) * 5/9 + 273.15).toFixed(2);
+      break;
+    case "kelvin":
+      celsiusEl.value = (currentValue - 273.15).toFixed(2);
+      fahrenheitEl.value = ((currentValue - 273.15) * 9/5 + 32).toFixed(2);
+      break;
+      default:
+        break;
+  }
+}
+
