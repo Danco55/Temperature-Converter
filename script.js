9ae71247a2b3a495f5dcb0a8fa037c9fc142d9f9
Daniel <136647628+Danco55@users.noreply.github.com>
2024-01-25 23:42:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const celsiusEl = document.getElementById("celsius");
 const fahrenheitEl = document.getElementById("fahrenheit");
 const kelvinEl = document.getElementById("kelvin"); 
 
+
+
 function computeTemp(event) {
   const currentValue = +event.target.value;
 
